eaee2d99ab9c7c7f9992c706d258a58213cc80d8
rajaehtasham <rajaehtasham1921@gmail.com>
2026-08-17 22:51:15 +0500

    Add Tailwind CSS classes and placeholders to SignupForm field widgets

--- bazaar-marketplace/core/forms.py
+++ bazaar-marketplace/core/forms.py
@@ -8,6 +8,19 @@ class SignupForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
     username=forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder' : ' Your username',
-
+        'placeholder' : 'Your username',
+        'class' : 'w-full py-4 px-6 rounded-xl'
     }))
+    email=forms.CharField(widget=forms.EmailInput(attrs={
+            'placeholder' : 'Your email address',
+            'class' : 'w-full py-4 px-6 rounded-xl'
+        }))
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={
+            'placeholder' : 'Your password',
+            'class' : 'w-full py-4 px-6 rounded-xl'
+        }))
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={
+            'placeholder' : 'Repeat password',
+            'class' : 'w-full py-4 px-6 rounded-xl'
+        }))
+    
